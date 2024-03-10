ad0423027151874810fcc32044265c35b5f267cf
iTakedown27 <ezrabridger27@gmail.com>
2024-03-10 00:31:33 -0800

    restructure files

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static('UI'));
 app.use('/css', express.static(__dirname + "/UI/CSS"));
-app.use('/html', express.static(__dirname + "/UI/HTML"));
+app.use('/html', express.static(__dirname + "//HTML"));
 app.use('/img', express.static(__dirname + "/UI/images"));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/UI/HTML/index.html');
+app.get('/index.html', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/', (req, res) => {
+app.post('/index.html', (req, res) => {
     
 });
 
 app.get('/map.html', (req, res) => {
-    res.sendFile(__dirname + '/UI/HTML/map.html');
+    res.sendFile(__dirname + '//HTML/map.html');
 });
 
 app.post('/map.html', (req, res) => {
@@ -24,7 +24,7 @@ app.post('/map.html', (req, res) => {
 });
 
 app.get('/analytics.html', (req, res) => {
-    res.sendFile(__dirname + '/UI/HTML/analytics.html');
+    res.sendFile(__dirname + '//HTML/analytics.html');
 });
 
 app.post('/analytics.html', (req, res) => {
@@ -32,7 +32,7 @@ app.post('/analytics.html', (req, res) => {
 });
 
 app.get('/settings.html', (req, res) => {
-    res.sendFile(__dirname + '/UI/HTML/settings.html');
+    res.sendFile(__dirname + '//HTML/settings.html');
 });
 
 app.post('/settings.html', (req, res) => {
@@ -40,7 +40,7 @@ app.post('/settings.html', (req, res) => {
 });
 
 app.get('/sign-in.html', (req, res) => {
-    res.sendFile(__dirname + '/UI/HTML/sign-in.html');
+    res.sendFile(__dirname + '//HTML/sign-in.html');
 });
 
 app.post('/sign-in.html', (req, res) => {
